7224ce5b74a14c67a35ac2784baddaa15d7e999f
DurtDiv3r <75084231+DurtDiv3r@users.noreply.github.com>
2023-02-24 14:57:31 +1300

    MaterialComposeReader: Initial Hilt setup

---
+++ app/src/main/java/com/example/materialcomposereader/MaterialComposeReaderApplication.kt
@@ -0,0 +1,7 @@
+package com.example.materialcomposereader
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MaterialComposeReaderApplication : Application()
