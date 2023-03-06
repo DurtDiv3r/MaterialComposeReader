73271edaadce9cf1a6d739a8199784bc0ab8265a
DurtDiv3r <75084231+DurtDiv3r@users.noreply.github.com>
2023-03-07 09:15:22 +1300

    MaterialComposeReader: Login DI refactor of Service.

--- app/src/main/java/com/example/materialcomposereader/model/service/LoginService.kt
+++ app/src/main/java/com/example/materialcomposereader/model/service/LoginService.kt
@@ -1,15 +1,18 @@
 package com.example.materialcomposereader.model.service
 
+import com.example.materialcomposereader.model.User
+import kotlinx.coroutines.flow.Flow
+
 interface LoginService {
     val currentUserId: String
-
-    suspend fun authenticate(email: String, password: String, route: (Boolean, String) -> Unit)
+    val hasUser: Boolean
+    val currentUser: Flow<User>
+    suspend fun authenticate(email: String, password: String)
 
     suspend fun createUser(
         userName: String,
         email: String,
         password: String,
-        route: (Boolean, String) -> Unit,
     )
 
     suspend fun signOut()
--- app/src/main/java/com/example/materialcomposereader/model/service/impl/LoginServiceImpl.kt
+++ app/src/main/java/com/example/materialcomposereader/model/service/impl/LoginServiceImpl.kt
@@ -1,7 +1,11 @@
 package com.example.materialcomposereader.model.service.impl
 
+import com.example.materialcomposereader.model.User
 import com.example.materialcomposereader.model.service.LoginService
 import com.google.firebase.auth.FirebaseAuth
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.tasks.await
 import javax.inject.Inject
 
@@ -9,33 +13,28 @@ class LoginServiceImpl @Inject constructor(private val auth: FirebaseAuth) : Log
     override val currentUserId: String
         get() = auth.currentUser?.uid.orEmpty()
 
+    override val hasUser: Boolean
+        get() = auth.currentUser != null
+
+    override val currentUser: Flow<User>
+        get() = callbackFlow {
+            val listener =
+                FirebaseAuth.AuthStateListener { auth ->
+                    this.trySend(auth.currentUser?.let { User(it.uid) } ?: User())
+                }
+            auth.addAuthStateListener(listener)
+            awaitClose { auth.removeAuthStateListener(listener) }
+        }
+
     override suspend fun authenticate(
         email: String,
         password: String,
-        route: (Boolean, String) -> Unit,
     ) {
-        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener {
-            if (it.isSuccessful) {
-                route(true, "${it.result?.user?.email} logged in successfully")
-            } else {
-                route(false, "Login unsuccessful: ${it.exception?.message}")
-            }
-        }.await()
+        auth.signInWithEmailAndPassword(email, password).await()
     }
 
-    override suspend fun createUser(
-        userName: String,
-        email: String,
-        password: String,
-        route: (Boolean, String) -> Unit,
-    ) {
-        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
-            if (it.isSuccessful) {
-                route(true, "User created successfully")
-            } else {
-                route(false, "Create User Unsuccessful: ${it.result}")
-            }
-        }.await()
+    override suspend fun createUser(userName: String, email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password).await()
     }
 
     override suspend fun signOut() {
